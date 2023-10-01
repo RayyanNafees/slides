7efeaf7cb8305f2a4e3f7b83cee436a60efdb709
RayyanNafees <nafees.rayyan@gmail.com>
2023-10-01 14:48:11 +0530

    Added Express Midleware integration

---
+++ run-server.mjs
@@ -0,0 +1,15 @@
+import express from 'express'
+import { handler as ssrHandler } from './dist/server/entry.mjs'
+
+const app = express()
+app.use(express.static('dist/client/'))
+app.use((req, res, next) => {
+  const locals = {
+    title: 'New title',
+  }
+
+  ssrHandler(req, res, next, locals)
+})
+
+app.get('/home', (req, res) => res.send('Rayyno Nafeeso'))
+app.listen(8080)
